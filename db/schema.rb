6a106bbfdc6307b55218f05450d250808c60f557
jcheahh <jackcheah2@gmail.com>
2021-03-23 20:58:52 +0800

    Setup test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_044829) do
+ActiveRecord::Schema.define(version: 2021_03_23_095809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "task", null: false
