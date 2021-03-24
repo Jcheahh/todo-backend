4a8a7adf87b46428dd5dfb538ad1d1054545bdde
jcheahh <jackcheah2@gmail.com>
2021-03-24 13:23:02 +0800

    Finish test

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,5 @@
 class Todo < ApplicationRecord
+   validates_presence_of :task
+   validates :is_done, inclusion: { in: [ true, false ] }
+
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Todo, type => :model do
-  describe "when Todo Model is valid" do
+RSpec.describe Todo, :type => :model do
+  subject { described_class.new(task: "Swimming", is_done: false) }
+  it "is valid when Todo Model is valid" do
+    expect(subject).to be_valid
+  end
+
+  it "it not valid without a task" do
+    subject.task = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "it not valid without is_done" do
+    subject.is_done = nil
+    expect(subject).to_not be_valid
   end
 end
