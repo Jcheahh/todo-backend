27fe50b821683a7322bb264f7c5fb5ad285e1fa5
jcheahh <jackcheah2@gmail.com>
2021-04-01 15:22:11 +0800

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   match '*all', controller: 'application', action: 'cors_preflight_check', via: [:options]
+  
+  scope :api, defaults: { format: :json } do
+    # devise_for :users, only: :sessions
+    devise_for :users
+    as :user do
+      post 'signin', to: 'devise/sessions#create', as: :user_session
+      delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    end
+  end
 
   root "todos#index"
 
   resources :todos
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
