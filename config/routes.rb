a5f23956bcb3975a96a632bcf2bb69b0f40246d7
jcheahh <jackcheah2@gmail.com>
2021-04-02 21:49:57 +0800

    Update devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
   match '*all', controller: 'application', action: 'cors_preflight_check', via: [:options]
-  
+
   scope :api, defaults: { format: :json } do
-    # devise_for :users, only: :sessions
-    devise_for :users
-    as :user do
-      post 'signin', to: 'devise/sessions#create', as: :user_session
-      delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
-    end
+    devise_for :users,
+               controllers: { registrations: 'api/registrations', sessions: 'api/sessions' }
   end
 
-  root "todos#index"
+  root 'todos#index'
 
   resources :todos
 end
