925aa366545122fdbb303443a5207f0137c75faf
jcheahh <jackcheah2@gmail.com>
2021-04-02 21:59:42 +0800

    Add registration controller

---
+++ app/controllers/api/registrations_controller.rb
@@ -0,0 +1,14 @@
+class Api::RegistrationsController < Devise::RegistrationsController
+  def create
+    user = User.new(sign_up_params)
+
+    if user.save
+      sign_in(resource_name, user)
+      token = current_user.generate_jwt
+      render json: { token: token }
+    else
+      # TODO: user.errors.full_messages
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+end
