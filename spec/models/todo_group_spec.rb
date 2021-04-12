ff1598aec60fad20368add7bdc997feae5c4baf3
jcheahh <jackcheah2@gmail.com>
2021-04-12 23:21:12 +0800

    Finish todo group model test case

--- spec/models/todo_group_spec.rb
+++ spec/models/todo_group_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
 RSpec.describe TodoGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  subject { described_class.new(title: "Task of today", user_id: user.id) }
+  it "is valid when TodoGroup Model is valid" do
+    expect(subject).to be_valid
+  end
+
+  it "is invalid without a title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is invalid without a user" do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
+  end
 end
