72da5f72d672821ebd034cbdaab5af08b3d6b654
jcheahh <jackcheah2@gmail.com>
2021-08-07 23:05:03 +0800

    Add user field to create and add validate token endpoint to render current user

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,12 +1,18 @@
 class Api::SessionsController < Devise::SessionsController
+  before_action :authenticate_user!, only: [:validate_token]
+
   def create
     user = User.find_by_email!(sign_in_params[:email])
 
     if user && user.valid_password?(sign_in_params[:password])
       token = current_user.generate_jwt
-      render json: { token: token }
+      render json: { token: token, user: current_user }
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def validate_token
+    render json: current_user
   end
 end
